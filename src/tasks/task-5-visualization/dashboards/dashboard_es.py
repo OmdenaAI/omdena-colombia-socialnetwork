fbf5a60ccfd11143e735a21863ec30095977530a
casalazara <ca.salazara@uniandes.edu.co>
2021-09-27 23:08:59 -0500

    moved from directory visualization files, added dashboard in english and emojis cloud

---
+++ src/tasks/task-5-visualization/dashboards/dashboard_es.py
@@ -0,0 +1,80 @@
+
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+import matplotlib.pyplot as plt
+from utils.funs import (load_data, get_hashtag_counts,
+                        get_emojicloud, get_emojis_by_week)
+
+
+st.title("Colombia social network analysis during strike")
+st.markdown("-by Omdena")
+st.sidebar.title("Sentiment Analysis of Tweets/Reddit messages")
+st.markdown("Streamlit based interactive dashboard used ")
+st.sidebar.markdown("Streamlit based interactive dashboard used "
+                    "to analyze sentiments of tweets 🐦 and Reddit")
+
+data = load_data("es")
+hashtag_counts = get_hashtag_counts(data)
+emoji_cloud = get_emojicloud()
+
+# Random tweet by sentiment
+st.sidebar.subheader("Display random tweet")
+random_tweet = st.sidebar.radio('Sentiment', ('POS', 'NEU', 'NEG'))
+st.sidebar.markdown(data.query(
+    "full_text_sentiment == @random_tweet")[["full_text"]].sample(n=1).iat[0, 0])
+
+# Tweets by sentiment
+st.sidebar.markdown("### Total Number of tweets by sentiment")
+select = st.sidebar.selectbox('Type of visualization', [
+    'Bar plot', 'Pie chart'], key='1')
+sentiment_count = data['full_text_sentiment'].value_counts()
+sentiment_count = pd.DataFrame(
+    {'Sentiment': sentiment_count.index, 'Tweets': sentiment_count.values})
+
+# move to plotting
+if not st.sidebar.checkbox("Hide", True):  # by defualt hide the checkbar
+    st.markdown("### Number of tweets by sentiment")
+    if select == 'Bar plot':
+        fig = px.bar(sentiment_count, x='Sentiment',
+                     y='Tweets', color='Tweets', height=500)
+        st.plotly_chart(fig)
+    else:
+        fig = px.pie(sentiment_count, values='Tweets', names='Sentiment')
+        st.plotly_chart(fig)
+
+# Tweets per hashtag
+st.sidebar.subheader("Total number of tweets for each hashtag")
+each_hashtag = st.sidebar.selectbox(
+    'Type of visualization', ['Bar plot', 'Pie chart'], key='2')
+hashtags_to_consider = st.sidebar.slider(
+    "Hashtags to look at", 0, len(hashtag_counts))
+hashtags = [hashtag for hashtag in hashtag_counts.keys()]
+tweets = [hashtag for hashtag in hashtag_counts.values()]
+hashtag_sentiment_count = pd.DataFrame(
+    {'Hashtag': hashtags[:hashtags_to_consider], 'Tweets': tweets[:hashtags_to_consider]})
+if not st.sidebar.checkbox("Close", True, key='21'):
+    if each_hashtag == 'Bar plot':
+        st.subheader("Total number of tweets for each hashtag")
+        fig_1 = px.bar(hashtag_sentiment_count, x='Hashtag',
+                       y='Tweets', color='Tweets', height=500)
+        st.plotly_chart(fig_1)
+    if each_hashtag == 'Pie chart':
+        st.subheader("Total number of tweets for each hashtag")
+        fig_2 = px.pie(hashtag_sentiment_count,
+                       values='Tweets', names='Hashtag')
+        st.plotly_chart(fig_2)
+
+# Emojis wordcloud
+st.sidebar.subheader("Emojis by week")
+weeks_to_consider = st.sidebar.slider(
+    "Hashtags to look at", 1, int(data["preprocessed_created_at"].dt.week.max()))
+emojis = get_emojis_by_week(data, weeks_to_consider)
+if not st.sidebar.checkbox("Close", True, key='31'):
+    st.subheader("Emoji cloud for each week")
+    fig, ax = plt.subplots()
+    wc = emoji_cloud.generate(emojis)
+    im = ax.imshow(wc.recolor(color_func=emoji_cloud.color_func, random_state=42),
+                   interpolation="bilinear")
+    ax.axis("off")
+    st.pyplot(fig)
---
+++ src/tasks/task-5-visualization/dashboards/utils/funs.py
@@ -0,0 +1,55 @@
+
+
+import pandas as pd
+import streamlit as st
+from utils.emojis import EmojiCloud
+
+FILE_PATH = "src/data/task-5-visualization/Corrected_final_all_with_sentiment.csv"
+FONTS_PATH = "src/tasks/task-5-visualization/dashboards/utils/fonts"
+
+
+def clean_emojis(emoji):
+    emoji = emoji.replace("[","")
+    emoji = emoji.replace("]","")
+    emoji = emoji.replace("'","")
+    emoji = emoji.strip()
+    return emoji 
+
+@st.cache(persist=True)
+def load_data(lang):
+    data = pd.read_csv(FILE_PATH)
+    data['preprocessed_created_at'] = pd.to_datetime(
+        data['preprocessed_created_at'])
+    data = data[data["twitter_lang"] == lang]
+    sentiments = {
+        1: "POS",
+        0: "NEU",
+        -1: "NEG"
+    }
+    data["full_text_sentiment"] = data["sentiment_score"].map(sentiments)
+    return data
+
+@st.cache(persist=True)
+def get_hashtag_counts(data):
+    ans = {}
+    for _, row in data.iterrows():
+        if not pd.isna(row["hashtags"]):
+            for hashtag in row["hashtags"].split(" , "):
+                ans[hashtag] = ans.get(hashtag.strip(), 0) + 1
+    ans = {k: v for k, v in sorted(
+        ans.items(), key=lambda item: item[1], reverse=True)}
+    return ans
+
+@st.cache(persist=True)
+def get_emojicloud():
+    return EmojiCloud(font_path=f"{FONTS_PATH}/Symbola.ttf")
+
+@st.cache(allow_output_mutation=True)
+def get_emojis_by_week(data, weeks: int):
+    first_week = data["preprocessed_created_at"].dt.week.min()
+    rows = data[data["preprocessed_created_at"].dt.week < (weeks + first_week)]
+    ans = []
+    for i in rows.emoji_list:
+        if i != "['']":
+            ans.append(clean_emojis(i))
+    return ans
